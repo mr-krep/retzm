396170b6e783a9835247ccb627df97d779766364
mr-krep <79119507079@ya.ru>
2024-11-02 16:54:24 +0300

    Скролл для брендов переведен на css

--- js/custom.js
+++ js/custom.js
@@ -16,14 +16,3 @@ const swiper_banner = new Swiper('.banner', {
     prevEl: '.banner__nav_buttons-prev',
   },
 });
-
-// const swiper_brands = new Swiper('.brands', {
-//   // autoplay: {delay: 5000},
-//   slidesPerView: 8,
-//   // spaceBetween: 20,
-//   loop: true,
-//   // lazyPreloadPrevNext: 1,
-//   wrapperClass: 'brands__list',
-//   slideClass: 'brands__list__item',
-//   // slideActiveClass: 'brands__list__item--active',
-// });
