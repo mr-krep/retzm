d5a802b5bb326302aaf846a615bbbba64b6c6775
mr-krep <79119507079@ya.ru>
2024-11-22 01:08:31 +0300

    Черновой вариант главной страницы. Отказ от слайдера в слайдере, отладка всех слайдеров, адаптив.

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,3 @@
-// const swiper_banner = new Swiper('.banner', {
-//   autoplay: {delay: 5000},
-//   loop: true,
-//   lazyPreloadPrevNext: 1,
-//   wrapperClass: 'banner__container',
-//   slideClass: 'banner__container__item',
-//   slideActiveClass: 'banner__container__item--active',
-//   pagination: {
-//     el: '.banner__pagination',
-//     bulletClass: 'banner__pagination__bullet',
-//     bulletActiveClass: 'banner__pagination__bullet--active',
-//     clickable: true
-//   },
-//   navigation: {
-//     nextEl: '.banner__nav_buttons-next',
-//     prevEl: '.banner__nav_buttons-prev',
-//   },
-// });
-
 var slider = tns({
   container: '.banner__container',
   items: 1,
@@ -41,25 +22,12 @@ var slider = tns({
 
 var slider = tns({
   container: '.suggestions__container__list',
-  nested: 'outer',
+  fixedWidth: 262,
   items: 5,
   loop: false,
   mouseDrag: true,
   nav: false,
   controls: true,
-  controlsContainer: '.suggestions__container__nav_buttons'
+  controlsContainer: '.suggestions__container__nav_buttons',
+  gutter: 10
 });
- 
-var product_sliders = document.getElementsByClassName('suggestions__container__list__item__images');
-for (var i=0; i< product_sliders.length; i++ ) {
-  var slider = tns({
-    container: '#' + product_sliders[i].id,
-    nested: 'inner',
-    items: 1,
-    mouseDrag: false,
-    controls: false,
-    nav: true,
-    navAsThumbnails: true,
-    navContainer: '#' + product_sliders[i].id + '-dots',
-  });
-}
