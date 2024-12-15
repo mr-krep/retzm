1e80420b96910d4796d7533d321a771b085d553e
mr-krep <79119507079@ya.ru>
2024-12-16 02:33:15 +0300

    Страница О нас

---
+++ js/about.js
@@ -0,0 +1,17 @@
+var slider = tns({
+  container: '.about_utp__container',
+  responsive: {
+    "840": {
+      items: 3,
+      autoWidth: true
+    }
+  },
+  items: 1.5,
+  // autoWidth: true,
+  // fixedWidth: 262,
+  loop: false,
+  mouseDrag: true,
+  nav: false,
+  controls: false,
+  // gutter: 10
+});
