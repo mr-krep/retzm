11dae5281f8683615c353c2f6c58ab362c5c9b65
mr-krep <79119507079@ya.ru>
2025-01-09 01:19:08 +0300

    Страница: Категория (2й уровень) Кроме слейдера картинок товара, фильтров/сортировки в mobile

---
+++ js/catalogue.js
@@ -0,0 +1,11 @@
+// var slider = tns({
+//   container: '#product-slider-2',
+//   // fixedWidth: 200,
+//   items: 1,
+//   loop: true,
+//   mouseDrag: true,
+//   nav: true,
+//   navAsThumbnails: true,
+//   navContainer: '#product-slider-2 > .catalogue__products__list__item__images__link__navcontainer',
+//   controls: false
+// });
