b1c0832532b41d259a48539be8a6ef6e6e4528a3
mr-krep <79119507079@ya.ru>
2025-01-27 23:55:06 +0300

    Товар - html разметка

---
+++ js/product.js
@@ -0,0 +1,35 @@
+var slider = tns({
+  container: '.carousel_alike',
+  responsive: {
+    "840": {
+      fixedWidth: 262,
+      items: 5
+    }
+  },
+  fixedWidth: 200,
+  items: 2,
+  loop: false,
+  mouseDrag: true,
+  nav: false,
+  controls: true,
+  controlsContainer: '.carousel_alike__arrows',
+  gutter: 10
+});
+
+var slider = tns({
+  container: '.carousel_addon',
+  responsive: {
+    "840": {
+      fixedWidth: 262,
+      items: 5
+    }
+  },
+  fixedWidth: 200,
+  items: 2,
+  loop: false,
+  mouseDrag: true,
+  nav: false,
+  controls: true,
+  controlsContainer: '.carousel_addon__arrows',
+  gutter: 10
+});
