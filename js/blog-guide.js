7fef74bd865ea20110dc720ebcd57506a3d63836
mr-krep <79119507079@ya.ru>
2025-03-18 01:55:19 +0300

    Блог - страница Инструкция, desktop

---
+++ js/blog-guide.js
@@ -0,0 +1,11 @@
+var slider = tns({
+  container: '.banner__container',
+  items: 1,
+  lazyload: true,
+  mouseDrag: true,
+  controls: true,
+  nav: true,
+  navAsThumbnails: true,
+  navContainer: '.banner__dots',
+  controlsContainer: '.banner__arrows'
+});
